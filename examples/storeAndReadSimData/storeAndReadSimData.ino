4f1fa0df679c1ca8496a9fb96f4150b4cf51c69e
phlpjo <42817460+phlpjo@users.noreply.github.com>
2020-09-23 17:15:04 +0200

    Update storeAndReadSimData.ino

--- examples/storeAndReadSimData/storeAndReadSimData.ino
+++ examples/storeAndReadSimData/storeAndReadSimData.ino
@@ -18,100 +18,47 @@
   created 23. Sep 2020
   by Philip Decker
 */
-
-
-#include "Modem.h"
 #include "MkrGsm1400IoT.h"
+#include "arduino_secrets.h"
 
 
-String response;
+unsigned long lastMillis = 0;
+uint16_t      dataToWrite, 
+              readData;
+int unlockSimCard;              
 
+void setup() {
 
-void resetUblox() {
-  pinMode(GSM_RESETN, OUTPUT);
-  digitalWrite(GSM_RESETN, HIGH);
-  delay(100);
-  digitalWrite(GSM_RESETN, LOW);
+  Serial.begin(115200);
+  SerialGSM.begin(115200);      //using the SerialGSM from modem.h from the arduino-libraries/MKRGSM
+
+  resetUblox();                                 // reset the ublox module
+  delay(8000);                                  // waiting for ublox module coming up
+  unlockSimCard = unlockSim(SECRET_PINNUMBER);  // unlock sim and get return
+  if(unlockSimCard == 1){                       //unlock sim to access phonebook for store and read data - returns 1: sim ready; returns -1: wrong pin ; returns -2: need puk ; returns -3: other error
+    Serial.println("sim unlocked");   
+  }else{
+    Serial.println(unlockSimCard);
+      
+  }
+  delay(5000);                      //waiting for ublox module is unlocked
 }
 
-int unlockSim(String pin) {
-  MODEM.sendf("AT+CPIN=\"%s\"", pin.c_str());
-  if (MODEM.waitForResponse(10000) == 1) {
-    return 1; //sim unlocked
-  }
-  int checkUp = checkSimStatus();
-  if (checkUp == -1) {
-    return -1; //wrong pin
-  }
-  if (checkUp == -2) {
-    return -2; //need puk
-  }
-  return -3;
-}
+void loop() {
+  if (lastMillis + 5000 < millis()) {
+    lastMillis = millis();
 
 
+    writeSim(1,dataToWrite);    // write uint16_t to the simcard phonebook - writeSim(uint16_t adress, uint16_t data) - adress: 0...250, data:0...32 767 - returns -1: write error; returns 1: success
+    Serial.print("write to sim: ");
+    Serial.println(dataToWrite);
+    
+    readData = readSim(1);      // read uint16_t from the simcard phonebook - readSim(uint16_t adress) - returns -2: sim not ready; returns -1: wrong adress; 
+    Serial.print("data from sim: ");
+    Serial.println(readData);
 
-int checkSimStatus () {
-  for (unsigned long start = millis(); (millis() - start) < 1000;) {
-    MODEM.send("AT+CPIN?");
-    if (MODEM.waitForResponse(10000, &response) == 1) {
-      if (response.startsWith("+CPIN: ")) {
-        if (response.endsWith("READY")) {
-          return 1; //SIM ready
-        } else if (response.endsWith("SIM PIN")) {
-          return -1; // sim pin required
-        } else if (response.endsWith("SIM PUK")) {
-          return -2; // sim puk required
-        } else {
-          return -3; // other fail.
-        }
-      }
-    }
-  }
-}
+    dataToWrite++;  //to show data write and read works
 
-
-int writeSim (uint8_t adress, uint16_t input_data_uint16_t) {
-  if (checkSimStatus() == 1) {
-    MODEM.sendf("AT+CPBW=%d,\"%d\",,\"%d\"", adress, input_data_uint16_t, input_data_uint16_t );
-    if (MODEM.waitForResponse(10000, &response) == 1) {
-      if (response.lastIndexOf("OK\r\n")) {
-        return 1; //successfull write data to phonebook
-      } else {
-        return -1;  //error to write data
-      }
-    }
-  } else {
-    return -2; //sim not ready
-  }
-}
-
-
-int readSim (uint8_t adress) {
-  char data_string[50];
-  int i1, i2, i3, i4;
-  if (checkSimStatus() == 1) {
-    MODEM.sendf("AT+CPBR=%d", adress);
-    if (adress > 250 || adress < 0) {
-      if (Serial.available()) {
-        Serial.println("insert a valid address 0 ... 250 ");
-      }
-    }
-    if (MODEM.waitForResponse(10000, &response) == 1) {
-      if (response.startsWith("+CPBR:")) {
-        response.toCharArray(data_string, 50);
-        if (4 == sscanf(data_string,
-                        "%*[^0123456789]%d%*[^0123456789]%d%*[^0123456789]%d%*[^0123456789]%d",
-                        &i1,
-                        &i2,
-                        &i3,
-                        &i4))
-        {
-          return i2;
-        }
-      }
-    }
-  } else if (Serial.available()) {
-    Serial.println("sim not ready");
+    delay(2000);  //delay for the output on Serial monitor and to protect sim memory.
   }
 }
