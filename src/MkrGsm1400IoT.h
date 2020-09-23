5cf270b6f2dea608441e5970b9fe1a39cc43e1c5
phlpjo <42817460+phlpjo@users.noreply.github.com>
2020-09-23 17:14:19 +0200

    Update MkrGsm1400IoT.h

--- src/MkrGsm1400IoT.h
+++ src/MkrGsm1400IoT.h
@@ -1,3 +1,23 @@
+/*
+  This file is part of the MkrGsm1400IoT library.
+  It depends on the MKR GSM library.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+  
+  23. Sep. 2020
+  Author: Philip Decker
+*/
+
+
 #ifndef MkrGsm1400IoT_H
 #define MkrGsm1400IoT_H
 #include <Arduino.h>
