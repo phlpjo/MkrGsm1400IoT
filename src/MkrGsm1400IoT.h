fbe99137291668f333d675e73c0e7d7a067bbd8f
phlpjo <42817460+phlpjo@users.noreply.github.com>
2020-09-23 11:25:39 +0200

    Update MkrGsm1400IoT.h

--- src/MkrGsm1400IoT.h
+++ src/MkrGsm1400IoT.h
@@ -1,1 +1,14 @@
+#ifndef MkrGsm1400IoT_H
+#define MkrGsm1400IoT_H
+#include <Arduino.h>
 
+
+void    resetUblox();
+
+int     unlockSim(String pin),
+        checkSimStatus(), 
+        writeSim(uint8_t adress, int input_data),
+        readSim(uint8_t adress);
+
+
+#endif
